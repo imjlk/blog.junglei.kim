1bc7d8d059c508fc8d6e9445bdd22c15af494805
imjlk <i@m.junglei.kim>
2021-10-29 21:41:11 +0900

    test build action

--- packages/material-ui/src/components/index.js
+++ packages/material-ui/src/components/index.js
@@ -21,6 +21,8 @@ const Root = ({ state }) => {
         <Link link="/material-demo">Material Demo</Link>
         <br />
         <Link link="/material-tabs">Material Demo</Link>
+        <br />
+        <Link link="#">TEST</Link>
       </nav>
       <hr />
       <main>
