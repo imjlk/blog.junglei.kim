5aa6e448f5c46a832e581ab1055a360ff452af59
imjlk <i@m.junglei.kim>
2021-10-31 21:15:57 +0900

    post, page 컴포넌트 추가

--- packages/material-ui/src/components/index.js
+++ packages/material-ui/src/components/index.js
@@ -1,12 +1,20 @@
+import "dayjs/locale/ko"
+
 import Link from "@frontity/components/link"
+import List from "./List";
 import MaterialDemo from './material-demo'
 import MaterialTabs from './material-tabs'
+import Page from "./Page"
+import Post from './Post'
 import React from "react"
 import Switch from "@frontity/components/switch"
 import { connect } from "frontity"
+import dayjs from "dayjs"
 
 const Root = ({ state }) => {
+  dayjs.locale('ko');
   const data = state.source.get(state.router.link)
+
   return (
     <>
       <h1>Blog</h1>
@@ -14,22 +22,14 @@ const Root = ({ state }) => {
       <nav>
         <Link link="/">Home</Link>
         <br />
-        <Link link="/page/2">More posts</Link>
-        <br />
         <Link link="/about-us">About Us</Link>
-        <br />
-        <Link link="/material-demo">Material Demo</Link>
-        <br />
-        <Link link="/material-tabs">Material Demo</Link>
       </nav>
       <hr />
       <main>
         <Switch>
-          <div when={data.isArchive}>This is a list</div>
-          <div when={data.isPost}>This is a post</div>
-          <div when={data.isPage}>This is a page</div>
-          <div when={data.isMaterialDemo}><MaterialDemo /></div>
-          <div when={data.isMaterialTabs}><MaterialTabs /></div>
+          <List when={data.isArchive} />
+          <Post when={data.isPost} />
+          <Page when={data.isPage} />
         </Switch>
       </main>
     </>
