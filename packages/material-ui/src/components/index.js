f4e11d27410ac1f0c1ba4ca4fc3dae75c2ebee88
imjlk <i@m.junglei.kim>
2021-11-13 01:41:18 +0900

    google web font 추가

--- packages/material-ui/src/components/index.js
+++ packages/material-ui/src/components/index.js
@@ -1,22 +1,32 @@
 import "dayjs/locale/ko"
 
+import { Global, connect, css, styled, useConnect } from "frontity"
+
 import Link from "@frontity/components/link"
 import List from "./List";
-import MaterialDemo from './material-demo'
-import MaterialTabs from './material-tabs'
 import Page from "./Page"
 import Post from './Post'
 import React from "react"
 import Switch from "@frontity/components/switch"
-import { connect } from "frontity"
 import dayjs from "dayjs"
 
 const Root = ({ state }) => {
   dayjs.locale('ko');
+  // const { state } = useConnect(Packages);
   const data = state.source.get(state.router.link)
+
 
   return (
     <>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;400;700&display=swap');
+
+          html {
+            font-family: 'Noto Sans KR', sans-serif;
+          }
+        `}
+      />
       <h1>Blog</h1>
       <p>Current URL: {state.router.link}</p>
       <nav>
@@ -37,3 +47,5 @@ const Root = ({ state }) => {
 }
 
 export default connect(Root)
+
+const Header = styled.header`  background-color: #e5edee;`
